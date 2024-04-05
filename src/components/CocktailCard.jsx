24ec876b4ce5014f337792908b81f528067d6a3b
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-04-05 22:45:32 +0100

    Add all drinks returned value to cocktailed Card in order to display values accordingly and also links to each card single page.

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/CocktailCard"
 
-
-const CocktailCard = () => {
+const CocktailCard = ({id,image,name,info,glass}) => {
   return (
-    <h1>CocktailCard</h1>
+    <Wrapper>
+        <div className="img-container">
+            <img src={image} alt={name} />
+        </div>
+
+        <div className="footer">
+            <h4>{name}</h4>
+            <h5>{glass}</h5>
+            <p>{info}</p>
+            {/* The Id is making the Link goto Cocktail page that displays each details of drinks on a single page */}
+            <Link to={`/cocktail/${id}`} className="btn">Details</Link>
+
+        </div>
+    </Wrapper>
   )
 }
 
