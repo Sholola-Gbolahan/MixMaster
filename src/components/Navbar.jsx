b874a888b142d37e1c7a1e47d3346acbcb12e57b
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-04-05 07:46:15 +0100

    Styled Component initiated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  background: red;
+  padding: 1rem;
+  color: white;
+  font-size: 2rem; 
+`
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="nav-center">
-            <span className="logo">MixMaster</span>
+      <StyledBtn>Button Press</StyledBtn>
+      <div className="nav-center">
+        <span className="logo">MixMaster</span>
 
-            <div className="nav-links">
-                {/* Navlinks is used in linking to different pages */}
-                <NavLink to ='/'>Home</NavLink>
-                <NavLink to ='/about'>About</NavLink>
-                <NavLink to ='/newsletter'>Newsletter</NavLink>
-            </div>
+        <div className="nav-links">
+          {/* Navlinks is used in linking to different pages */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/newsletter">Newsletter</NavLink>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
