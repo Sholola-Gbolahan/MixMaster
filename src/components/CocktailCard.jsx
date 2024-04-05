2d920031e4a16246874fa903a1cf22948dc23fd9
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-04-05 23:32:17 +0100

    Initited Global loading and context

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,23 +1,32 @@
-import { Link } from "react-router-dom"
-import Wrapper from "../assets/wrappers/CocktailCard"
+import { Link, useOutletContext } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
 
-const CocktailCard = ({id,image,name,info,glass}) => {
+const CocktailCard = ({id, image, name, info, glass}) => {
+
+  // useOutletContext is used to give access to global values in all the cards
+  const data = useOutletContext();
+  console.log(data);
+
+
   return (
     <Wrapper>
-        <div className="img-container">
-            <img src={image} alt={name} />
-        </div>
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
 
-        <div className="footer">
-            <h4>{name}</h4>
-            <h5>{glass}</h5>
-            <p>{info}</p>
-            {/* The Id is making the Link goto Cocktail page that displays each details of drinks on a single page */}
-            <Link to={`/cocktail/${id}`} className="btn">Details</Link>
-
-        </div>
+      <div className="footer">
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        {/* The Id is making the Link goto Cocktail page that displays each details of drinks on a single page */}
+        <Link to={`/cocktail/${id}`} className="btn">
+          Details
+        </Link>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CocktailCard
+
+
+export default CocktailCard;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 Navbar
 const HomeLayout = () => {
+
+  // useNavigation Give us info about the State
+  const navigation = useNavigation()
+
+  console.log(navigation)
+
+  const isPageLoading = navigation.state === 'loading';
+  // Creating a variable for global context
+ const value = "Some values"
   return (
     <div>
       <Navbar />
       <section className="page">
-      <Outlet />
+        {isPageLoading ? ( <div className="loading" />) 
+        : 
+        // Accessing global context in all our pages by using Context props
+         ( <Outlet context = {{value}} /> ) }
+ 
       </section>
       
 
