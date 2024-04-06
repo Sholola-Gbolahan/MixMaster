898ed73fc8ef59d1d3f0a2a7b2d1ee258fa1ff69
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-04-06 01:28:03 +0100

    Coctail single page setup y loading and display required data

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,28 +1,76 @@
 import { useLoaderData, Link } from "react-router-dom";
 import axios from "axios";
-import Wrapper from '../assets/wrappers/CocktailPage'
+import Wrapper from "../assets/wrappers/CocktailPage";
 
+const singleCocktailUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
-const singleCocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
-
-// Params give the ID result 
-export const loader = async ({params}) => {
-  
+// Params give the ID result
+export const loader = async ({ params }) => {
   // grabing id value from params and storing it into a new object namd id
-  const {id} = params
-// decomposing data from the response
-  const {data} = await axios.get(`${singleCocktailUrl}${id}`)
+  const { id } = params;
+  // decomposing data from the response
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
 
-
-  return {id, data}
-}
+  return { id, data };
+};
 
 const Cocktail = () => {
   //  This is used to render the values in the browser
-  const {id,data} = useLoaderData() 
-  return (
-    <h1>Cocktail</h1>
-  )
-}
+  const { id, data } = useLoaderData();
 
-export default Cocktail 
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  return (
+    <Wrapper>
+
+      <header>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </header>
+
+      <div className="drink">
+
+        <img src={image} alt={name} className="img" />
+
+        <div className="drink-info">
+        <p>
+          {" "}
+          <span className="drink-data"> Name : </span> {name}{" "}
+        </p>
+        <p>
+          {" "}
+          <span className="drink-data"> Category :</span> {category}{" "}
+        </p>
+        <p>
+          {" "}
+          <span className="drink-data"> Info :</span> {info}
+        </p>
+        <p>
+          {" "}
+          <span className="drink-data"> Glass : </span> {glass}
+        </p><p>
+          {" "}
+          <span className="drink-data"> Instructions : </span> {instructions}
+        </p>
+        
+      </div>
+
+      </div>
+
+     
+    </Wrapper>
+  );
+};
+
+export default Cocktail;
